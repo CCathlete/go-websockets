edf56ce8203956eb854be994c596dd7862190847
CCat <ccathlete01@gmail.com>
2025-01-20 09:42:27 +0100

    Implemented the db connection (driver) and the entity + aggregate for email messages.

--- internal/channel-data/mail-data.go
+++ internal/channel-data/mail-data.go
@@ -1,1 +1,29 @@
 package channelData
+
+import "html/template"
+
+// An entity that holds info for sending an email.
+type MailData struct {
+	ToName,
+	ToAddress,
+	FromName,
+	FromAddress,
+	Subject,
+	Template string
+
+	AdditionalTo,
+	CC,
+	Attachments []string
+
+	Content   template.HTML
+	UseHermes bool
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
+	RowSets   map[string]any
+}
+
+// An aggregate that actions will be performed on.
+type MailJob struct {
+	MailMessage MailData
+}
--- internal/db-driver/driver.go
+++ internal/db-driver/driver.go
@@ -2,18 +2,53 @@ package driver
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-// Holds the db connection object.
+// Holds a db connection.
 type DB struct {
 	*sql.DB
 }
 
-var dbConn = &DB{}
-
 const (
-	maxOpenDbConn = 25
-	maxIdleDbConn = 25
-	maxDbLifetime = 5 * time.Minute
+	maxOpenDbConns = 25
+	maxIdleDbConns = 25
+	maxDbLifetime  = 5 * time.Minute
 )
+
+// A factory for a db connection.
+func ConnectToPostgres(connectionString string,
+) (dbConn *DB, err error) {
+
+	// --------------- Creating a new db connection --------------------
+
+	db, err := sql.Open("pgx", connectionString)
+	if err != nil {
+		err = fmt.Errorf("couldn't create a db connection: %w", err)
+		return
+	}
+
+	db.SetMaxOpenConns(maxOpenDbConns)
+	db.SetMaxIdleConns(maxIdleDbConns)
+	db.SetConnMaxLifetime(maxDbLifetime)
+	dbConn = &DB{
+		DB: db,
+	}
+
+	// -------------------- Pinging the db -----------------------------
+
+	err = dbConn.Ping()
+	if err != nil {
+		err = fmt.Errorf("created a db connection but couldn't ping: %w", err)
+	} else {
+		log.Println("*** Pinged DB successfully ***")
+	}
+
+	return
+}
