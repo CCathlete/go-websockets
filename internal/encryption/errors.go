9e1db60f0e376afdda32f690098499cd4a270699
CCat <ccathlete01@gmail.com>
2025-01-23 05:42:02 +0100

    Changed error types in encryption to error wrapper functions.

--- internal/encryption/errors.go
+++ internal/encryption/errors.go
@@ -6,8 +6,23 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Wraps the error returned by the database with a custom error.
+type RepoError func(error) error
+
+// Wraps the error returned by the database with a custom error.
 var (
-	ErrPasswordHasing   = fmt.Errorf("error hashing password")
-	ErrPAsswordEncoding = fmt.Errorf("error encoding password")
-	ErrPasswordMismatch = bcrypt.ErrMismatchedHashAndPassword
+	ErrPasswordHasing RepoError = func(err error) error {
+		return fmt.Errorf("error hashing password: %w", err)
+	}
+
+	ErrPAsswordEncoding RepoError = func(err error) error {
+		return fmt.Errorf("error encoding password: %w", err)
+	}
+
+	ErrPasswordMismatch RepoError = func(err error) error {
+		return fmt.Errorf("%w: %w",
+			bcrypt.ErrMismatchedHashAndPassword,
+			err,
+		)
+	}
 )
